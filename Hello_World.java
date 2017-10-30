c330f158986d095f98c3767b376de2fc8e0110a6
Matthew Burnes <1burnesmat@hdsb.ca>
2017-10-30 16:10:05 -0400

    Add files via upload

---
+++ Hello_World.java
@@ -0,0 +1,15 @@
+// The "Hello_World" class.
+import java.awt.*;
+import hsa.Console;
+
+public class Hello_World
+{
+    static Console c;           // The output console
+    
+    public static void main (String[] args)
+    {
+	c = new Console ();
+	
+       c.println ("Hello World");
+    } // main method
+} // Hello_World class
